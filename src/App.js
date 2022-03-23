0c6dd2abe6fe872c6fbb6c6b8264967534482479
Roeck <rons9n@gmail.com>
2022-03-23 02:15:08 -0400

    Directory component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import CategoryItem from './components/Category/CategoryItem'
+import Directory from './components/Directory/Directory';
 
 const App = () => {
   const categories = [
@@ -29,13 +29,7 @@ const App = () => {
     }
   ]
 
-  return (
-    <div className='categories-container'>
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
-  );
+  return <Directory categories={categories} />;
 };
 
 export default App;
