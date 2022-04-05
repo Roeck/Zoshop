adc597f54360250230f1e22ff686f8e4d1834dfc
Roeck <rons9n@gmail.com>
2022-04-05 14:07:42 -0400

    Update item quantity on icon

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,14 +7,14 @@ import { CartContext } from '../../contexts/Cart.context';
 import './cart.styles.scss';
 
 const Cart = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
   
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
   
   return (
     <div className='icon-container' onClick={toggleIsCartOpen}>
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>0</span>
+      <span className='item-count'>{cartCount}</span>
     </div>
   );
 };
