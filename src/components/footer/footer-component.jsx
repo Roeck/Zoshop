24f67b67f7cccfe2555992ef2848224f3748d13a
Roeck <rons9n@gmail.com>
2022-04-13 20:40:29 -0400

    Fixed footer

--- src/components/footer/footer-component.jsx
+++ src/components/footer/footer-component.jsx
@@ -5,7 +5,7 @@ import './footer.styles.scss';
 const Footer = () => {
   return (
     <div className='footer'>
-      <h5>Copyright &#169; <a href="https://github.com/Roeck/Karted">Zoshop</a></h5>
+      <h5>Copyright &#169; <a href="https://github.com/Roeck/Zoshop">Zoshop</a></h5>
     </div>
   )
 }
